f4ab2f3ede3010b5cc827146ed539ff802777f62
adwyxx <adwyxx@qq.com>
2019-09-23 23:56:59 +0800

    redis 分布式锁

--- api/godzilla-oauth/src/main/java/com/adwyxx/godzilla/oauth/lock/RedisDistributedLock.java
+++ api/godzilla-oauth/src/main/java/com/adwyxx/godzilla/oauth/lock/RedisDistributedLock.java
@@ -3,7 +3,15 @@ package com.adwyxx.godzilla.oauth.lock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalField;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Description: Redis分布式锁实现
@@ -36,5 +44,51 @@ public class RedisDistributedLock {
     public void unlock(){
 
 
+    }
+
+    /**
+     * @description: 获取分布式锁
+     * @author: Leo.Wang
+     * @date: 2019/9/23 23:08
+     * @param lock: 锁信息
+     * @param timeout: 超时时间
+     * @param tryInterval: 尝试获取锁的时间间隔
+     * @param lockExpireTime : 锁失效时间
+     * @return boolean: 是否成功获取锁
+     */
+    public boolean getLock(RedisLock lock,long timeout,long tryInterval,long lockExpireTime)
+    {
+        try{
+           if(lock == null || StringUtils.isEmpty(lock.getKey()) || StringUtils.isEmpty(lock.getValue())){
+               return false;
+            }
+
+            long startTime = System.currentTimeMillis();
+            ValueOperations<String,String> opts = null;
+            do{
+                //锁存在
+                if(redisTemplate.hasKey(lock.getKey())){
+                    logger.debug("lock is exist!");
+                }
+                else{
+                    //生成锁
+                    opts = redisTemplate.opsForValue();
+                    opts.set(lock.getKey(),lock.getValue(),lockExpireTime, TimeUnit.MILLISECONDS);
+                    return true;
+                }
+                //获取锁超时
+                if(System.currentTimeMillis()-startTime>timeout){
+                    logger.debug("lcok is expired!");
+                    return false;
+                }
+
+                TimeUnit.MILLISECONDS.sleep(tryInterval);
+
+            } while (redisTemplate.hasKey(lock.getKey()));
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+        return false;
     }
 }
